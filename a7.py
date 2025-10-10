0a47cb7a7271ea197af6016333c4ac07377090e2
Asanov Arseny <asanov.aa0009@gmail.com>
2025-10-10 12:39:03 +0300

    Добавил файл a7.py

---
+++ a7.py
@@ -0,0 +1,11 @@
+x1 = int(input("Введите x1: "))
+y1 = int(input("Введите y1 "))
+x2 = int(input("Введите x2 "))
+y2 = int(input("Введите y2 "))
+if (x1 + y1) % 2 == 0 and (x2 + y2) % 2 == 0:
+    print("YES\nWhite")
+elif (x1 + y1) % 2 != 0 and (x2 + y2) % 2 != 0:
+    print("YES\nBlack")
+
+else:
+    print("NO")
